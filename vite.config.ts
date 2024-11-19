927e8fa894984c231a72d61f3597151a3e72533a
Christian Marc Galingan <124617174+Xtiannn07@users.noreply.github.com>
2024-11-19 15:49:10 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "https://xtiannn07.github.io/portfolio-v2/" : "/", // Replace "repository-name" with your GitHub repo name
+  base: mode === "production" ? "/portfolio-v2/" : "/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    target: "esnext",
+  },
 }));
-
